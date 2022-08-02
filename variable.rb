a76108d06e1f605c7526d7d1217a83c0edef93ab
Lacey Weaver <jlweave89@gmail.com>
2022-08-02 15:50:52 -0600

    Adding comments

--- variable.rb
+++ variable.rb
@@ -1,14 +1,15 @@
+# string
 email = "rubyrulz@gmail.com"
 user_id = "coding4life!"
-
+# integer
 age = 33
 date = 822022
-
+# floats
 price = 1.25
 tax = 0.66
-
+#boolean
 x = TRUE
-
+#puts
 puts email
 puts user_id
 puts age
