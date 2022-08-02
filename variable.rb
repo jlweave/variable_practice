dfa24993d34abcc0699e3d57c662e70e65e379df
Lacey Weaver <jlweave89@gmail.com>
2022-08-02 15:26:23 -0600

    Fixing coding issues maybe

--- variable.rb
+++ variable.rb
@@ -1,15 +1,14 @@
 Variables
-# string
+
 email = "rubyrulz@gmail.com"
 user_id = "coding4life!"
-# integers
+
 age = 33
 date = 822022
-# floats
+
 price = 1.25
-tax = .66
-#booleans
+tax = 0.66
+
 x = 1
 x == 1
 x != 1
-#
